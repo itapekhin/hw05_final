d994134ad91224286380c0c1d48d0d4d68ae3cb5
Тапехин Иван <ivan-tapehin@yandex.ru>
2021-12-23 22:55:39 +0300

    спринт6.1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -58,34 +58,35 @@ class Post(models.Model):
 
 class Comments(models.Model):
     post = models.ForeignKey(Post,
-			     on_delete=models.CASCADE,  
-			     related_name='comments',
+                 on_delete=models.CASCADE,  
+                 related_name='comments',
                  blank=True,
-                 null=True,) 
+                 null=True,
+    ) 
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        )
-    text = models.CharField(max_length=200)  
-    created = models.DateTimeField(auto_now_add=True)  
-      
-    class Meta:  
-        ordering = ('created',)  
-          
+    )
+    text = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('created',)
+
     def __str__(self):
         return 'Comment by {} on {}'.format(self.text, self.post)
+
 
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='follower'
-        )
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='following'
-        )
+    )
     def __str__(self):
         return f'Подписчик {self.user}, Автор {self.author}'
-    
