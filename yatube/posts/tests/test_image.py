08df86a0817646fcac8f0206b57cc40b23f88591
Тапехин Иван <ivan-tapehin@yandex.ru>
2021-12-24 18:35:36 +0300

    спринт555

--- yatube/posts/tests/test_image.py
+++ yatube/posts/tests/test_image.py
@@ -13,7 +13,7 @@ TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
 @override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class ImageTests(TestCase):
-    
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
