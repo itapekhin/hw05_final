d9532f0d3a60b99e3bd07eb428aec18fc035b8ce
Тапехин Иван <ivan-tapehin@yandex.ru>
2021-12-24 20:07:59 +0300

    спринт555

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,6 +21,8 @@ from django.conf.urls.static import static
 
 
 handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('auth/', include('users.urls', namespace='users')),
