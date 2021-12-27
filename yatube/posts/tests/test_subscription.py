0aad97a9b531e79adc0c816c63d395a27bcf4f5e
Тапехин Иван <ivan-tapehin@yandex.ru>
2021-12-27 21:57:52 +0300

    Спринт6_itog

--- yatube/posts/tests/test_subscription.py
+++ yatube/posts/tests/test_subscription.py
@@ -66,7 +66,7 @@ class SubscriptionTests(TestCase):
             'posts:follow_index')
         )
         first_object = response.context['page_obj'].object_list[0]
-        self.assertEqual(first_object, SubscriptionTests.post1)
+        self.assertEqual(first_object, SubscriptionTests.post2)
 
     def test_subscription_unfollow(self):
         self.authorized_author.get(reverse(
