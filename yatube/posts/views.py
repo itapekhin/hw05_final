c9eb3a16a737b090b82ce980772355b29fef6025
Тапехин Иван <ivan-tapehin@yandex.ru>
2021-12-28 11:37:48 +0300

    спринт6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -35,7 +35,7 @@ def profile(request, username):
             user=user, author=author
         ).exists()
     elif not user.is_authenticated or Follow.objects.filter(
-            user=user,author=user):
+            user=user, author=user):
         following = None
     context = {
         'profile_count': profile_count,
